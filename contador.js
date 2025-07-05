07cfb70cd9c027f284565d3f91c1dfbe2adf78c1
Yasmimos <yasmimoliveira1403677@gmail.com>
2025-07-05 11:34:17 -0300

    ex4pt3

---
+++ contador.js
@@ -0,0 +1,12 @@
+console.log('Contador usando While:')
+let i = 1;
+
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+console.log('Contador usando For:')
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+  
